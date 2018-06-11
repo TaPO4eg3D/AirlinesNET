e608acbe9981776893e5f13f950da83d550cc577
TaPO4eg3D <fo.denis2011@gmail.com>
2018-06-11 12:53:10 +0700

    Flights now can be edited

---
+++ AirlinesNET/AdminControl/EditFlight.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AirlinesNET.Models;
+
+namespace AirlinesNET.AdminControl
+{
+    /// <summary>
+    /// Interaction logic for EditFlight.xaml
+    /// </summary>
+    public partial class EditFlight : Window
+    {
+
+        private DataContext db = new DataContext();
+        private int selectedFlight { get; set; }
+
+        /// <summary>
+        /// Fetch all companies
+        /// </summary>
+        private void fetchCompanies()
+        {
+            var companies = db.Companies.Where(c => c.Name.Contains(companySearch.Text)).ToList();
+            companyGrid.ItemsSource = companies;
+        }
+
+        /// <summary>
+        /// Fetch all airports
+        /// </summary>
+        private void fetchAirports()
+        {
+            var airports = db.Airports.Where(c => c.Name.Contains(pointsSeacrh.Text) || c.Country.Contains(pointsSeacrh.Text) || c.City.Contains(pointsSeacrh.Text)).ToList();
+            startPointGrid.ItemsSource = airports;
+            endPointGrid.ItemsSource = airports;
+        }
+
+        public EditFlight(int selectedFlight)
+        {
+            InitializeComponent();
+            this.selectedFlight = selectedFlight;
+            fetchCompanies();
+            fetchAirports();
+
+            // Select current values
+            var _selectedFlight = db.Flights.Where(f => f.FlightID == selectedFlight).FirstOrDefault();
+            var selectedCompany = _selectedFlight.Company;
+            var selectedStartPoint = _selectedFlight.Airport; 
+            var selectedEndPoint = _selectedFlight.Airport1;
+
+            companyGrid.SelectedItem = selectedCompany;
+            startPointGrid.SelectedItem = selectedStartPoint;
+            endPointGrid.SelectedItem = selectedEndPoint;
+            seatsField.Text = _selectedFlight.Seats.ToString();
+
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var flight = db.Flights.Where(f => f.FlightID == selectedFlight).FirstOrDefault();
+
+            Company company = (Company)companyGrid.SelectedItem;
+            Airport startPoint = (Airport)startPointGrid.SelectedItem;
+            Airport endPoint = (Airport)endPointGrid.SelectedItem;
+            int seats;
+            try
+            {
+                seats = Convert.ToInt32(seatsField.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Вы не ввели количество сидений!");
+                return;
+            }
+             
+            flight.CompanyID = company.CompanyID;
+            flight.StartPoint = startPoint.AirportID;
+            flight.EndPoint = endPoint.AirportID;
+            flight.Seats = seats;
+
+            db.SaveChanges();
+
+            MessageBox.Show("Успешно!");
+            this.Close();
+        }
+
+        // Triggers search of Airports
+        private void pointsSeacrh_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            fetchAirports();
+        }
+
+        // Triggers search of Companies
+        private void companySearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            fetchCompanies();
+        }
+    }
+}
---
+++ AirlinesNET/AdminControl/Flights.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AirlinesNET.Models;
+
+namespace AirlinesNET.AdminControl
+{
+    /// <summary>
+    /// Interaction logic for Users.xaml
+    /// </summary>
+    public partial class Flights : Window
+    {
+
+        private DataContext db = new DataContext();
+
+        /// <summary>
+        /// Fetch data from database, include user's filter
+        /// </summary>
+        private void fetchData()
+        {
+            string searchText = searchField.Text;
+            var data = from p in db.Flights.AsNoTracking()
+                       where p.Company.Name.Contains(searchText) || p.Airport.Name.Contains(searchText) || p.Airport.Country.Contains(searchText) || p.Airport.City.Contains(searchText)
+                       select p;
+            mainGrid.ItemsSource = data.ToList();
+        }
+
+        /// <summary>
+        /// Triggers filter(searchField)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void searchField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            fetchData();
+        }
+
+        /// <summary>
+        /// Triggers filter(checkBox)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void checkBoxTrigger(object sender, RoutedEventArgs e)
+        {
+            fetchData();
+        }
+
+        public Flights()
+        {
+            InitializeComponent();
+            fetchData();
+        }
+
+        /// <summary>
+        /// Add new entity
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addEntity_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAirport createAirport = new CreateAirport();
+            createAirport.ShowDialog();
+            fetchData();
+        }
+
+        /// <summary>
+        /// Delete entity
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void deleteEntity_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedEntities = mainGrid.SelectedItems.Cast<Flight>().ToList();
+            if(selectedEntities.Count == 0)
+            {
+                MessageBox.Show("Выберите хотя бы одну компанию!");
+                return;
+            }
+
+            var confirmation = MessageBox.Show("Вы уверены, что хотите удалить выбраные компании?", "Подтвердите удаление", MessageBoxButton.YesNo);
+            if(confirmation == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            db.Flights.RemoveRange(selectedEntities);
+            db.SaveChanges();
+
+            fetchData();
+        }
+
+        /// <summary>
+        /// Manage editing grid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            try
+            {
+                var _selectedItem = (Flight)e.Row.Item;
+                var selectedItem = db.Flights.Where(p => p.FlightID == _selectedItem.FlightID).FirstOrDefault();
+
+                selectedItem = _selectedItem;
+                db.SaveChanges();
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// Edit entity
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void editEntity_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedFlight = (Flight)mainGrid.SelectedItem;
+            if(selectedFlight == null)
+            {
+                MessageBox.Show("Выберите рейс!");
+                return;
+            }
+            EditFlight editFlight = new EditFlight(selectedFlight.FlightID);
+            editFlight.ShowDialog();
+            fetchData();
+        }
+    }
+}
